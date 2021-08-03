034b9c6b0f84ca2b69f141f9c2621b1609daef76
titomatu <tamatu@gmail.com>
2021-08-02 19:12:17 -0500

    mend

--- src/java/io/tamatu/springmvc/WebSpringMVCController.java
+++ src/java/io/tamatu/springmvc/WebSpringMVCController.java
@@ -5,11 +5,8 @@
  */
 package io.tamatu.springmvc;
 
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
